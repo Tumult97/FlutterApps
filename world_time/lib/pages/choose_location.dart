f4bb2dd30f80e93c4e0709053e0556aeb5f1e745
Tristan <tristanvdm87@gmail.com>
2020-10-11 21:21:46 +0200

    Lesson 34
    Listview builder

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -7,7 +8,16 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  int counter = 0;
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flagUrl: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flagUrl: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flagUrl: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flagUrl: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flagUrl: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flagUrl: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flagUrl: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flagUrl: 'indonesia.png'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +30,24 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 20.0,
       ),
-      body: SafeArea(
-          child: RaisedButton(
-            onPressed: () {
-              setState(() {
-                counter++;
-              });
-            },
-            child: Text("$counter"),
-          ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/${locations[index].flagUrl}"),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
